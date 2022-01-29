708fc3e936bc421275966c09d414180152996a5f
thgdanilaya <frederigo234@outlook.com>
2022-01-29 13:18:40 +0300

    Initial commit

---
+++ src/first.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+import static java.lang.Math.*;
+
+public class first {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ans = 0;
+        int[] v = new int[10];
+        for (int i = 0; i < 10; ++i) {
+            v[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < 10; ++i) {
+            if (v[i] == 1) {
+                int len = 10;
+                for (int j = 0; j < 10; ++j) {
+                    if (v[j] == 2) {
+                        if (abs(j - i) < len) {
+                            len = abs(j - i);
+                        }
+                    }
+                }
+                if (len > ans && len != 10) {
+                    ans  = len;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
