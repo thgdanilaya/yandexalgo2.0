d172ac8858ed9082bed556ba3039c6a2c8b8987b
thgdanilaya <frederigo234@outlook.com>
2022-01-30 14:32:24 +0300

    fixed, solution is good

--- src/second_lesson/third.java
+++ src/second_lesson/third.java
@@ -3,34 +3,29 @@ package second_lesson;
 import java.util.Scanner;
 
 
-
 public class third {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s;
-        boolean flag = false; // если длина слова четная то 1, если нет то 0
+        int flag; // если длина слова четная то 1, если нет то 0
         int ukaz1, ukaz2; // указатели
         int ans = 0;
         s = scanner.next();
         int len = s.length();
-        if (len % 2 == 1) {
-            flag = true;
-        }
+        flag = len % 2;
         int mid = len / 2;
-        if (flag) {
-            ukaz1 = mid - 1;
-            ukaz2 = mid + 1;
+        if (flag == 1) {
+            ukaz1 = mid + 1;
         } else {
             ukaz1 = mid;
-            ukaz2 = mid-1;
         }
-        for (int i = 0; i < len / 2; ++i ) {
+        ukaz2 = mid - 1;
+        for (int i = 0; i < len / 2; ++i) {
             if (s.charAt(ukaz1) != s.charAt(ukaz2)) {
                 ans += 1;
             }
-            ukaz1 -=1;
-
-            ukaz2 +=1;
+            ukaz1 += 1;
+            ukaz2 -= 1;
         }
         System.out.println(ans);
     }
